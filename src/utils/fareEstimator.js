ad0d36a3edbd4c27630c35d1d05a3af019708708
J1309 <J1309@users.noreply.github.com>
2026-09-26 19:40:26 +0530

    Remove unused images and clean up assets to optimize codebase

--- src/utils/fareEstimator.js
+++ src/utils/fareEstimator.js
@@ -25,7 +25,7 @@ export const VEHICLE_OPTIONS = [
     capacity: 'Up to 7 Passengers',
     passengersMax: 7,
     luggageMax: 6,
-    image: '/images/suburban_actual.jpg',
+    image: '/images/fleet_suburban.jpg',
     minHours: 2,
     features: [
       '7 Full-Size Leather Executive Seats',
